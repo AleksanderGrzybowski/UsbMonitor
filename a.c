42d8b28a01586c8c73c1f80255b5d3730fb66627
Aleksander Grzybowski <some@email.com>
2016-03-03 22:28:25 +0100

    Working ADC 2 channels

--- a.c
+++ a.c
@@ -5,10 +5,40 @@
 
 #include "HD44780.h"
 
+void init_adc() {
+	// 1.1V reference
+	ADMUX |= (1<<REFS0) | (1<<REFS1);
+
+	// prescaler 128 and enable ADC 
+	ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0) | (1<<ADEN);    
+}
+
+uint16_t read_adc(uint8_t channel) {
+	ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
+
+	// single conversion mode
+	ADCSRA |= (1<<ADSC);
+
+	// wait until ADC conversion is complete
+	while(ADCSRA & (1<<ADSC));
+
+	return ADC;
+}
+
 int main(void) {
 
+	init_adc();
 	LCD_Initalize();
-	LCD_WriteText("Hello world");
 
-	while(1);
+	char disp[10];
+	int i = 0;
+
+	while(1) {
+		uint16_t first = read_adc(0);	
+		uint16_t second = read_adc(1);
+		sprintf(disp, "%d: %u - %u", i++, first, second);
+		LCD_WriteText(disp);
+		_delay_ms(1000);
+		LCD_Clear();
+	}
 }
