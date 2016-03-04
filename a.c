2df7bca476e1d81c0df83b77cb142a53eaeffec2
Aleksander Grzybowski <some@email.com>
2016-03-04 09:20:12 +0100

    Structure to hold data, view

--- a.c
+++ a.c
@@ -25,19 +25,36 @@ uint16_t read_adc(uint8_t channel) {
 	return ADC;
 }
 
+typedef struct meas {
+	float volts;
+	uint16_t milliamps;
+} meas;
+
+meas get_single() {
+	meas m;
+	m.volts = ((1.1 * read_adc(0))/1024.0) * (1000.0 + 4700.0) / (1000.0);
+	m.milliamps = (uint16_t) (((1.1 * read_adc(1))/1024.0) * 0.2 * 1000);
+	return m;
+}
+
 int main(void) {
 
 	init_adc();
 	LCD_Initalize();
 
-	char disp[17];
-	int i = 0;
+	char disp1[17];
+	char disp2[17];
 
 	while(1) {
-		float volts = ((1.1 * read_adc(0))/1024.0) * (1000.0 + 4700.0) / (1000.0);
-		uint16_t milliamps = (uint16_t) (((1.1 * read_adc(1))/1024.0) * 0.2 * 1000);
-		sprintf(disp, "%d: %.2f - %u", i++, volts, milliamps);
-		LCD_WriteText(disp);
+		meas m = get_single();
+		sprintf(disp1, "U = %.2f V", m.volts);
+		sprintf(disp2, "I = %u", m.milliamps);
+		LCD_GoTo(0, 0);
+		LCD_WriteText(disp1);
+		LCD_GoTo(0, 1);
+		LCD_WriteText(disp2);
+		LCD_GoTo(9, 1);
+		LCD_WriteText("mA");
 		_delay_ms(1000);
 		LCD_Clear();
 	}
